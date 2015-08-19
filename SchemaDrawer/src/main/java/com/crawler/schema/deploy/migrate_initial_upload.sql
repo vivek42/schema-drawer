b2e085feddc51544f778153b4911361d7f60693e
vivekchouhan <vchouhan@litle.com>
2015-08-18 23:09:20 -0400

    Added tables for persisting user

--- SchemaDrawer/src/main/java/com/crawler/schema/deploy/migrate_initial_upload.sql
+++ SchemaDrawer/src/main/java/com/crawler/schema/deploy/migrate_initial_upload.sql
@@ -15,3 +15,28 @@ CREATE TABLE Events (
   STACK_TRACE VARCHAR(45) NULL,
   APPLICATION_NAME VARCHAR(45) NULL,
   PRIMARY KEY (EVENT_ID));
+  
+create table users (
+  user_id int not null auto_increment,
+  username varchar(30) not null,
+  password varchar(50) not null,
+  create_date datetime not null default current_timestamp,
+  email varchar(50) not null,
+  primary key (user_id)
+  );
+  
+create table permission_ref (
+  permission_id int not null auto_increment,
+  name varchar(20) not null,
+  description varchar(50) not null,
+  primary key (permission_id)
+  );
+  
+create table user_permission_xref (
+  id int not null auto_increment,
+  permission_id int not null,
+  user_id int not null,
+  primary key (id),
+  foreign key (permission_id) references permission_ref (permission_id) on delete cascade,
+  foreign key (user_id) references users (user_id) on delete cascade
+  );
